efa8eb3d6e7aa7ac84b0cd53f10c96359c17d5f8
David Graeff <david.graeff@web.de>
2016-06-22 15:37:36 +0200

    The daily running update wakaama script would also benefit from a newer cmake. Fix buildsystem and travis-ci integration.
    Signed-off-by: David Graeff <david.graeff@web.de>

--- test/cmake.sh
+++ test/cmake.sh
@@ -1,18 +1,15 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
 
-# Download newer version of cmake. We need 3.1 at least, travis provides 2.8.
-CMAKE_URL="http://cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz"
-mkdir cmake
-curl -L ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
 export PATH=${TRAVIS_BUILD_DIR}/cmake/bin:${PATH}
 
 # GTest files
-mkdir ${GTEST_DIR}
+if [ ! -d "${GTEST_DIR}" ]; then
+    mkdir ${GTEST_DIR}
+fi
 cp -r /usr/src/gtest/* ${GTEST_DIR}
 
-USER_ID = $(id -u)
-sudo ip tuntap add dev tap0 mode tap user ${USER_ID}
-sudo ip tuntap add dev tap1 mode tap user ${USER_ID}
+sudo ip tuntap add dev tap0 mode tap user $(id -u)
+sudo ip tuntap add dev tap1 mode tap user $(id -u)
 sudo ip a a dev tap0 192.168.7.1/24
 sudo ip a a dev tap1 192.168.7.2/24
 
@@ -22,16 +19,18 @@ sudo brctl addif tap0tap1 tap1
 
 
 # Create a build dir and change to that.
-mkdir ${TRAVIS_BUILD_DIR}/buildtest
+if [ ! -d "${TRAVIS_BUILD_DIR}/buildtest" ]; then
+    mkdir ${TRAVIS_BUILD_DIR}/buildtest
+fi
 cd ${TRAVIS_BUILD_DIR}/buildtest
 
 # Execute cmake and build and test.
-cmake -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER ${TRAVIS_BUILD_DIR}/test/with_posix_sockets
+cmake -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER -DGTEST_DIR="${GTEST_DIR}" ${TRAVIS_BUILD_DIR}/test/with_posix_sockets
 make 
 make test
 rm-rf *
 
-cmake -DTAP_CLIENT_DEVICE="tap0" -DTAP_SERVER_DEVICE="tap1" -DTAP_SERVER_ADDR="192.168.7.2" -DTAP_CLIENT_ADDR="192.168.7.1" -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER ${TRAVIS_BUILD_DIR}/test/with_lwip
+cmake -DTAP_CLIENT_DEVICE="tap0" -DTAP_SERVER_DEVICE="tap1" -DTAP_SERVER_ADDR="192.168.7.2" -DTAP_CLIENT_ADDR="192.168.7.1" -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER -DGTEST_DIR="${GTEST_DIR}" ${TRAVIS_BUILD_DIR}/test/with_lwip
 make 
 make test
 
--- update_wakaama.cmake
+++ update_wakaama.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.1)
+cmake_minimum_required(VERSION 2.8)
 
 find_package(Git REQUIRED)
 
@@ -6,15 +6,22 @@ set(GH_REF "github.com/Openhab-Nodes/libWakaamaEmb.git")
 
 # update from external git
 file(REMOVE_RECURSE "tmp")
-file(REMOVE_RECURSE "src/wakaama")
-execute_process(COMMAND ${GIT_EXECUTABLE} clone https://github.com/eclipse/wakaama.git tmp --branch master --single-branch --depth 1)
+execute_process(COMMAND ${GIT_EXECUTABLE} clone https://${GH_TOKEN}@github.com/eclipse/wakaama.git tmp --branch master --single-branch --depth 1)
 set(REP "${CMAKE_CURRENT_LIST_DIR}/tmp")
 execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD OUTPUT_VARIABLE DESCRIBE_REVISION WORKING_DIRECTORY "${REP}" )
-file(GLOB_RECURSE WAKAAMA_FILES "tmp/core/*")
-file(COPY tmp/core DESTINATION src/)
-file(RENAME src/core src/wakaama)
-file(REMOVE_RECURSE "tmp")
+#file(GLOB_RECURSE WAKAAMA_FILES "tmp/core/*")
+if (EXISTS "${REP}/core")
+    message("Update wakaama to ${DESCRIBE_REVISION}")
+    file(REMOVE_RECURSE "src/wakaama")
+    file(COPY tmp/core DESTINATION src/)
+    file(RENAME src/core src/wakaama)
+    file(REMOVE_RECURSE "tmp")
+endif()
 
 execute_process(COMMAND ${GIT_EXECUTABLE} add src/wakaama)
 execute_process(COMMAND ${GIT_EXECUTABLE} commit --author="Wakaama Update <david.graeff@web.de>" -m "Update wakaama source to revision ${DESCRIBE_REVISION}")
-execute_process(COMMAND ${GIT_EXECUTABLE} push "https://${GH_TOKEN}@${GH_REF}" master:master  )
+
+execute_process(OUTPUT_VARIABLE OUT COMMAND ${GIT_EXECUTABLE} git diff HEAD origin/HEAD --quiet --)
+if (NOT ${OUT} EQUAL 0)
+    execute_process(COMMAND ${GIT_EXECUTABLE} push "https://${GH_TOKEN}@${GH_REF}" master:master  )
+endif()
