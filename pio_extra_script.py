3fafd29b5950c7288e1ef223295554b7a63e50f4
David Gräff <david.graeff@web.de>
2017-06-29 22:46:52 +1000

    extra_script.py fixed
    Signed-off-by: David Gräff <david.graeff@web.de>

--- pio_extra_script.py
+++ pio_extra_script.py
@@ -1,5 +1,5 @@
 from SCons.Script import DefaultEnvironment
 
-env = Environment()
-env.Append(CFLAGS=["-std=c11"])  # General options that are passed to the C compiler (C only; not C++).
+env = DefaultEnvironment()
+env.Append(CFLAGS=["-std=gnu11"])  # General options that are passed to the C compiler (C only; not C++).
 env.Append(CXXFLAGS=["-std=c++11"])  # General options that are passed to the C++ compiler. 
