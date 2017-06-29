e039a98e436a8c6b8adba7026739ad1e79b14cb6
David Gräff <david.graeff@web.de>
2017-06-29 22:58:49 +1000

    Travis CI: Gtest not found fixed
    Signed-off-by: David Gräff <david.graeff@web.de>

--- test/setup_gtest.sh
+++ test/setup_gtest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-if [ ! -d "${GTEST_DIR}" ]; then
+#if [ ! -d "${GTEST_DIR}" ]; then
     mkdir ${GTEST_DIR}
     if [ -d "/usr/src/gtest/" ]; then
+        echo "Copy GTest files to ${GTEST_DIR}"
         cp -r /usr/src/gtest/* ${GTEST_DIR}
+        cp -r /usr/include/gtest/* ${GTEST_DIR}
     fi
-fi
+#fi
